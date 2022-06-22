e811f95e9ac67a6d93a593bf8647aeca43f1c291
SianTharit <Tharit.preecha1@gmail.com>
2022-06-22 14:13:16 +0700

    nav

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,4 +1,21 @@
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
+import logo from "../../../assets/images/logo.png";
+
 function Navbar() {
-   return <div>Navbar</div>;
+   return (
+      <Box sx={{ flexGrow: 1 }}>
+         <AppBar position="sticky" color="light">
+            <Toolbar>
+               <IconButton>
+                  <img
+                     style={{ width: 43, height: 37 }}
+                     src={logo}
+                     alt="logo"
+                  />
+               </IconButton>
+            </Toolbar>
+         </AppBar>
+      </Box>
+   );
 }
 export default Navbar;
