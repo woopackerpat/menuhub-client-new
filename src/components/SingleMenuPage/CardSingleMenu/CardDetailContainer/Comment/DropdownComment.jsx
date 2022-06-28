bcfface92681b3954f9a60d538274d1958851415
Movie1103 <tpawarut@gmail.com>
2022-06-28 20:50:29 +0700

    feat: finished singleMenu(main card)

--- src/components/SingleMenuPage/CardSingleMenu/CardDetailContainer/Comment/DropdownComment.jsx
+++ src/components/SingleMenuPage/CardSingleMenu/CardDetailContainer/Comment/DropdownComment.jsx
@@ -2,10 +2,26 @@ import { useState } from 'react'
 import { Box, IconButton, Typography } from '@mui/material'
 import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded'
 import ArrowRightRoundedIcon from '@mui/icons-material/ArrowRightRounded'
-import CommmentContainer from './CommmentContainer'
+import CommmentContainer from './CommentContainer'
 
 function DropdownComment() {
   const [show, setShow] = useState(true)
+  const userComment = [
+    { userId: 1, user: 'John', profilePic: '', title: 'Nice' },
+    {
+      userId: 2,
+      user: 'Mike',
+      profilePic: '',
+      title: 'I like this restaurant',
+    },
+    { userId: 3, user: 'Tom', profilePic: '', title: 'Wowwwww!' },
+    {
+      userId: 4,
+      user: 'Alan',
+      profilePic: '',
+      title: 'The beef is sooo juicy',
+    },
+  ]
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
       <Box
@@ -17,7 +33,7 @@ function DropdownComment() {
         }}
       >
         <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-          3 comments
+          {userComment.length} comments
         </Typography>
         <IconButton onClick={() => setShow(prev => !prev)}>
           {show ? (
@@ -27,7 +43,7 @@ function DropdownComment() {
           )}
         </IconButton>
       </Box>
-      {show && <CommmentContainer />}
+      {show && <CommmentContainer userComment={userComment} />}
     </Box>
   )
 }
