224d0e4933000665fd5a5351edca3061eb472bf0
Movie1103 <tpawarut@gmail.com>
2022-06-29 20:04:49 +0700

    fix: bugs in comment

--- src/components/SingleMenuPage/CardSingleMenu/CardDetailContainer/UserAction.jsx
+++ src/components/SingleMenuPage/CardSingleMenu/CardDetailContainer/UserAction.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
-import ShareIcon from '@mui/icons-material/Share'
-import LinkIcon from '@mui/icons-material/Link'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import { Box, IconButton } from '@mui/material'
+import { useState } from "react"
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
+import ShareIcon from "@mui/icons-material/Share"
+import LinkIcon from "@mui/icons-material/Link"
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder"
+import FavoriteIcon from "@mui/icons-material/Favorite"
+import { Box, IconButton } from "@mui/material"
+import DropdownShare from "../../../common/cartItems/DropdownShare"
 
 function UserActionNavbar() {
   // like state for testing
@@ -13,42 +14,43 @@ function UserActionNavbar() {
   return (
     <Box
       sx={{
-        display: 'flex',
-        alignItems: 'center',
+        display: "flex",
+        alignItems: "center",
         gap: 1.5,
       }}
     >
       <IconButton
         sx={{
-          color: 'black',
+          color: "black",
         }}
       >
-        <MoreHorizIcon fontSize="large" />
+        <MoreHorizIcon fontSize='large' />
       </IconButton>
-      <IconButton
+      {/* <IconButton
         sx={{
           color: 'black',
         }}
       >
         <ShareIcon fontSize="large" />
-      </IconButton>
+      </IconButton> */}
+      <DropdownShare />
       <IconButton
         sx={{
-          color: 'black',
+          color: "black",
         }}
       >
-        <LinkIcon fontSize="large" />
+        <LinkIcon fontSize='large' />
       </IconButton>
       <IconButton
         sx={{
-          color: 'black',
+          color: "black",
         }}
         onClick={() => setIsLike(prev => !prev)}
       >
         {isLike ? (
-          <FavoriteIcon fontSize="large" color="error" />
+          <FavoriteIcon fontSize='large' color='error' />
         ) : (
-          <FavoriteBorderIcon fontSize="large" />
+          <FavoriteBorderIcon fontSize='large' />
         )}
       </IconButton>
     </Box>
--- src/components/SingleMenuPage/ContainerSingleMenu.jsx
+++ src/components/SingleMenuPage/ContainerSingleMenu.jsx
@@ -1,15 +1,17 @@
-import { Box } from '@mui/material'
-import MenuCardContainer from './CardSingleMenu/MenuCardContainer'
+import { Box } from "@mui/material"
+import HeaderAllMenusPage from "../AllMenusPage/HeaderAllMenusPage"
+import MenuCardContainer from "./CardSingleMenu/MenuCardContainer"
 
 function ContainerSingleMenu() {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
+        display: "flex",
+        flexDirection: "column",
         // gap: '20px',
       }}
     >
+      <HeaderAllMenusPage />
       <MenuCardContainer />
     </Box>
   )
