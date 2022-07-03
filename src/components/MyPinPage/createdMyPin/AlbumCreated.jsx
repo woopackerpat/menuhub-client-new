fdd3663fc5bb5607bf83657136140043c37d6845
Movie1103 <tpawarut@gmail.com>
2022-07-04 02:38:38 +0700

    back to develop to check for updates

--- src/components/MyPinPage/createdMyPin/AlbumCreated.jsx
+++ src/components/MyPinPage/createdMyPin/AlbumCreated.jsx
@@ -1,19 +1,19 @@
 import { Box, Typography } from "@mui/material";
 
 function AlbumCreated() {
-   return (
-      <Box
-         sx={{
-            display: "flex",
-            justifyContent: "center",
-            mt: 10,
-         }}
-      >
-         <Typography fontWeight="bold" variant="h4">
-            No created Pins yet
-         </Typography>
-      </Box>
-   );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        mt: 10,
+      }}
+    >
+      <Typography fontWeight="bold" variant="h4">
+        No created Pins yet
+      </Typography>
+    </Box>
+  );
 }
 
 export default AlbumCreated;
