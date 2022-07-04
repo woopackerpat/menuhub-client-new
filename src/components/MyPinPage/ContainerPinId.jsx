6698ed5acadade55a3ef041fe5c2346fccbfa58f
Movie1103 <tpawarut@gmail.com>
2022-07-04 20:24:08 +0700

    fix: edit .map in ContainerPinId

--- src/components/MyPinPage/ContainerPinId.jsx
+++ src/components/MyPinPage/ContainerPinId.jsx
@@ -24,7 +24,7 @@ function ContainerPinId() {
           columns={{ xs: 2, sm: 4, md: 4, lg: 5, xl: 7, xxl: 8 }}
           spacing={2}
         >
-          {pinById?.Restaurants.map(items => (
+          {pinById.Restaurants?.map(items => (
             <CartItemsRestaurant
               key={items?.id}
               Menus={items.Menus}
