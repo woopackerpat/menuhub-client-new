d999c73b5904d7f9329bfe3610b3876e70a19df7
SianTharit <Tharit.preecha1@gmail.com>
2022-07-07 14:05:01 +0700

    ready mui

--- src/components/common/ButtonSaveProfile.jsx
+++ src/components/common/ButtonSaveProfile.jsx
@@ -1,67 +1,68 @@
 import { LoadingButton } from "@mui/lab";
 import { useState } from "react";
 import { usePin } from "../../contexts/PinContextProvider";
-import { getAllPins } from "../../services/getAllPinsUnique";
 
 function ButtonSaveProfile({ onClick, loading, restaurantId, pinId }) {
-  const [deleting, setDeleting] = useState(false);
+   const [deleting, setDeleting] = useState(false);
 
-  const { pin, removeRestaurant } = usePin();
+   const { pin, removeRestaurant } = usePin();
 
-  const pins = pin?.filter(el => el.id === pinId);
+   const pins = pin?.filter((el) => el.id === pinId);
 
-  const isSaved = pins[0]?.Restaurants.findIndex(el => el.id === +restaurantId);
+   const isSaved = pins[0]?.Restaurants.findIndex(
+      (el) => el.id === +restaurantId
+   );
 
-  const handleRemoveRes = async () => {
-    try {
-      setDeleting(true);
-      await removeRestaurant(restaurantId, pinId);
-    } catch (err) {
-      console.log(err);
-    } finally {
-      setDeleting(false);
-    }
-  };
+   const handleRemoveRes = async () => {
+      try {
+         setDeleting(true);
+         await removeRestaurant(restaurantId, pinId);
+      } catch (err) {
+         console.log(err);
+      } finally {
+         setDeleting(false);
+      }
+   };
 
-  return (
-    <>
-      {isSaved === -1 ? (
-        <LoadingButton
-          loading={loading}
-          onClick={onClick}
-          variant="contained"
-          color="error"
-          sx={{
-            color: "white",
-            fontWeight: "bold",
-            lineHeight: "35px",
-            borderRadius: "24px",
-            textTransform: "none",
-          }}
-          disableElevation
-        >
-          Save
-        </LoadingButton>
-      ) : (
-        <LoadingButton
-          loading={deleting}
-          onClick={handleRemoveRes}
-          variant="contained"
-          color="primary"
-          sx={{
-            color: "white",
-            fontWeight: "bold",
-            lineHeight: "35px",
-            borderRadius: "24px",
-            textTransform: "none",
-          }}
-          disableElevation
-        >
-          Saved
-        </LoadingButton>
-      )}
-    </>
-  );
+   return (
+      <>
+         {isSaved === -1 ? (
+            <LoadingButton
+               loading={loading}
+               onClick={onClick}
+               variant="contained"
+               color="error"
+               sx={{
+                  color: "white",
+                  fontWeight: "bold",
+                  lineHeight: "35px",
+                  borderRadius: "24px",
+                  textTransform: "none",
+               }}
+               disableElevation
+            >
+               Save
+            </LoadingButton>
+         ) : (
+            <LoadingButton
+               loading={deleting}
+               onClick={handleRemoveRes}
+               variant="contained"
+               color="primary"
+               sx={{
+                  color: "white",
+                  fontWeight: "bold",
+                  lineHeight: "35px",
+                  borderRadius: "24px",
+                  textTransform: "none",
+               }}
+               disableElevation
+            >
+               Saved
+            </LoadingButton>
+         )}
+      </>
+   );
 }
 
 export default ButtonSaveProfile;
